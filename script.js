8a68ab2805da7cfdf9344e1f17110e65e9a89753
TheGenius777 <saiaish7777@gmail.com>
2026-08-11 15:23:12 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+async function searchQB() {
+    const answer = document.getElementById("answerInput").value;
+
+    if (!answer) {
+        alert("Please enter an answerline.");
+        return;
+    }
+
+    const url =
+        "https://www.qbreader.org/api/query" +
+        "?q=" + encodeURIComponent(answer) +
+        "&searchType=answer" +
+        "&questionType=all";
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const resultsDiv = document.getElementById("results");
+
+    resultsDiv.innerHTML = "";
+
+    const questions = [
+        ...data.tossups.questionArray,
+        ...data.bonuses.questionArray
+    ];
+
+    if (questions.length === 0) {
+        resultsDiv.innerHTML = "<p>No questions found.</p>";
+        return;
+    }
+
+    questions.forEach(question => {
+        const questionDiv = document.createElement("div");
+
+        questionDiv.innerHTML = `
+            <hr>
+            <p>${question.question}</p>
+            <strong>Answer: ${question.answer}</strong>
+        `;
+
+        resultsDiv.appendChild(questionDiv);
+    });
+}
