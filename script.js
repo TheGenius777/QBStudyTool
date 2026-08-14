98f1c46140778fce8bde963adc56c3e9b46d4733
TheGenius777 <saiaish7777@gmail.com>
2026-08-14 19:48:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -16,11 +16,15 @@ async function searchQB() {
         }
     });
 
+    const questionType = document.querySelector(
+    "#questionType input:checked"
+    ).value;
+
     let url =
-        "https://www.qbreader.org/api/query" +
-        "?q=" + encodeURIComponent(answer) +
-        "&searchType=answer" +
-        "&questionType=all";
+    "https://www.qbreader.org/api/query" +
+    "?q=" + encodeURIComponent(answer) +
+    "&searchType=answer" +
+    "&questionType=" + questionType;
 
     if (difficulties.length > 0) {
         url += "&difficulties=" + difficulties.join(",");
@@ -37,11 +41,26 @@ async function searchQB() {
 
     resultsDiv.innerHTML = "";
 
-    const questions = [
-        ...(data.tossups?.questionArray || [])
-    ].filter(function(question) {
-        return question && question.question;
-    });
+    let questions = [];
+
+    if (questionType === "tossup") {
+        questions = [
+            ...(data.tossups?.questionArray || [])
+        ];
+    }
+
+    else if (questionType === "bonus") {
+        questions = [
+            ...(data.bonuses?.questionArray || [])
+        ];
+    }
+
+    else {
+        questions = [
+        ...(data.tossups?.questionArray || []),
+        ...(data.bonuses?.questionArray || [])
+        ];
+    }
 
     if (questions.length === 0) {
         resultsDiv.innerHTML = "<p>No questions found.</p>";
@@ -49,15 +68,28 @@ async function searchQB() {
     }
 
     questions.forEach(function(question) {
-        const questionDiv = document.createElement("div");
+    const questionDiv = document.createElement("div");
 
+    if (question.question) {
+        // Tossup
         questionDiv.innerHTML =
             "<hr>" +
             "<p>" + question.question + "</p>" +
             "<strong>Answer: " +
             (question.answer_sanitized || question.answer || "Unknown") +
             "</strong>";
+    }
 
-        resultsDiv.appendChild(questionDiv);
-    });
+    else if (question.leadin) {
+        // Bonus
+        questionDiv.innerHTML =
+            "<hr>" +
+            "<p>" + question.leadin + "</p>" +
+            "<strong>Bonus Answers: " +
+            (question.answers_sanitized || question.answers || "Unknown") +
+            "</strong>";
+    }
+
+    resultsDiv.appendChild(questionDiv);
+});
 }
